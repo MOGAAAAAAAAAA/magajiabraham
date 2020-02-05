a1af83e29ddf0ce56ce97639e012167c4ecf26c8
MOGAAAAAAAAAA <60199005+MOGAAAAAAAAAA@users.noreply.github.com>
2020-02-05 15:08:50 -0600

    Add files via upload

---
+++ ex5.py
@@ -0,0 +1,16 @@
+# ex5.py
+my_name= 'Abraham Magaji'
+my_age= 18 #not a lie
+my_height= 90 #inches
+my_weight= 150 #lbs
+my_eyes= 'Hazelnut'
+my_teeth= 'White'
+my_hair= 'Black'
+
+print "let's talk about %s." %my_name
+print "He's %d inches tall." %my_height
+print "He's %d pounds heavy." %my_weight
+print "He's got %s eyes and %s hair." %(my_eyes,my_hair)
+print "His teeth are usually %s depending on the coffee." %my_teeth
+
+print "If I add %d, %d, and %d I get %d" %(my_age,my_height,my_weight,my_age+my_height+my_weight)
