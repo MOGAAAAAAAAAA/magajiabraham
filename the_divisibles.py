d226167fca22019948289e8065d8ecba3181e471
MOGAAAAAAAAAA <60199005+MOGAAAAAAAAAA@users.noreply.github.com>
2020-02-19 15:39:52 -0600

    Add files via upload

---
+++ the_divisibles.py
@@ -0,0 +1,15 @@
+for i in range(1,101):
+  if i%3 == 0:
+    print'divisible by 3 =',i
+  elif i%4 == 0:
+   print'divisible by 4=',i
+  elif i%5 == 0:
+    print'divisibl by 5=',i
+  elif i%6 == 0:
+    print'divisible by 6=',i
+  elif i%7 == 0:
+    print'divisible by 7=',i
+  elif i%8 == 0:
+    print'divisible by 8=',i
+  elif i%9 == 0:
+    print'divisible by 9',i
